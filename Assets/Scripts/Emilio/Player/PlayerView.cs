4f2879cbe9b2f05bc6d837989048dbdd168fd3b0
EmilioValente <32551483+EmilioValente@users.noreply.github.com>
2019-09-06 08:30:31 -0300

    primer instancia new player

--- Assets/Scripts/Emilio/Player/PlayerView.cs
+++ Assets/Scripts/Emilio/Player/PlayerView.cs
@@ -16,48 +16,65 @@ public class PlayerView : MonoBehaviour
         
     }
 
-    public void OnMove(bool isMoving)
+    public void OnMove(bool isMoving, bool isMovingBack)
     {
-
+        if (isMoving)
+        {
+            if (isMovingBack)
+                _myAnim.SetBool("isMovingBackwards", true);
+            else
+                _myAnim.SetBool("isMoving", true);
+        }
+        else
+        {
+            _myAnim.SetBool("isMoving", false);
+            _myAnim.SetBool("isMovingBackwards", false);
+        }
+        
+            
     }
 
     public void OnAttack(int slashNumbr)
     {
-
+        //particulas?
+        _myAnim.SetInteger("Slash", slashNumbr);
     }
 
     public void OnRangeAttack()
     {
-
+        _myAnim.SetTrigger("RangeAttack");
     }
 
     public void OnSpecial()
     {
-
+        _myAnim.SetTrigger("SpecialAttack");
     }
 
     public void OnDash()
     {
-
+        _myAnim.SetTrigger("Dash");
     }
 
     public void OnJump()
     {
-
+        _myAnim.SetTrigger("Jump");
     }
 
-    public void OnIdle()
+    public void OnIdle(bool isSlash)
     {
-
+        if (isSlash)
+            _myAnim.SetTrigger("AttackToIdle");
+        else
+            _myAnim.SetTrigger("ToIdle");
     }
 
     public void OnHit()
     {
-
+        _myAnim.SetTrigger("GotHit");
     }
 
     public void OnDeath()
     {
-
+        _myAnim.SetTrigger("Death");
     }
 }
